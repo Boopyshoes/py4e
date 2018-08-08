6a5f30858a8947f76f385a18723a30833afa7ea6
Dan Martz <djmartz77@gmail.com>
2018-08-07 22:17:42 -0500

    Finished Chapter 5

---
+++ py4e/chapter5/ex05_01.py
@@ -0,0 +1,18 @@
+total = 0
+count = 0
+
+while True:
+    num = input('Enter a number: ')
+    if num == 'done':
+        break
+    try:
+        num = int(num)
+    except:
+        print('bad data')
+        continue
+    total = total + num
+    count = count + 1
+
+print('The total is:',total)
+print('The count is:',count)
+print('The average is"',total / count)
