d84a84393c42dc3fe45923bb7c0f6f56f9815d98
Dan Martz <djmartz77@gmail.com>
2018-08-11 10:59:36 -0500

    finished chapter 13 Coursera exercise 1

---
+++ py4e/chapter13/ex13_coursera1.py
@@ -0,0 +1,25 @@
+# Python for Everyone
+# Chapter 13 Coursera Assignment 1
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter url to read - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+count = 0
+total = 0
+tags = soup('span')
+for tag in tags:
+    total += int(tag.contents[0])
+    count += 1
+
+print('Total:',total)
+print('Count',count)
